57853fe798dc781c2bca15d11b9cd2fff74d8eed
linkin <pablogermanjimenez7@gmail.com>
2025-07-15 02:37:51 -0300

    js con alto style man yeeeaah

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*Lista de tareas - Dificultad:  
+
+🟡🔴🟡
+ 
+3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda 
+cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, 
+cada elemento ingresado en la lista debe poder ser eliminado con un botón creado 
+para ese fin. */
